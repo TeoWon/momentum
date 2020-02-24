3a34c5c43cf9d7163bf7148a831c14d24492f5c4
Teo_won <teowon@naver.com>
2020-02-24 16:33:59 +0900

    Making a JS Clock

---
+++ clock.js
@@ -0,0 +1,18 @@
+const clockContainer = document.querySelector(".js-clock");
+const clcockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+  const date = new Date();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const seconds = date.getSeconds();
+  clcockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}
+  :${minutes < 10 ? `0${minutes}` : minutes}
+  :${seconds < 10 ? `0${seconds}` : seconds}`;
+  // 10s 보다 작으면 앞에 문자열 0이 같이 나오게
+}
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+}
+init();
