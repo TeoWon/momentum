551dc4320328f9189460d95b3a4409a8704a3ce3
Won taewoong <wontw@siliconcube.co.kr>
2020-03-04 18:25:54 +0900

    Weather (API)

--- weather.js
+++ weather.js
@@ -1,4 +1,22 @@
+//openweathermap에서 API Key값을 가져옴
+const API_KEY = "b7ef82467df9d9433ecff225335d6899";
 const COORDS = "coords";
+
+const weather = document.querySelector(".js-weather");
+
+function getWeather(latitude, longitude) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+  )
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCorrds(coordsObj) {
   //저장값은 string
@@ -14,6 +32,7 @@ function handleGeoSucces(position) {
     longitude: longitude
   };
   saveCorrds(coordsObj);
+  getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -30,6 +49,8 @@ function loadCoords() {
   if (loadedCoords === null) {
     askForCoords();
   } else {
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
   }
 }
 
